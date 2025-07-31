b719be0e77b50c9b95aa31e2267761c1436bf461
Saad Haji <hajisaad029@gmail.com>
2025-07-31 08:35:09 +0530

    Implemented real-time features using LiveBlocks

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -6,18 +6,6 @@ import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs';
 const Document = () => {
   return (
     <div>
-        <Header>
-            <div className="flex w-fit items-center justify-center gap-2">
-                <p className="document-title">Share</p>
-            </div>
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-        </Header>
-        <Editor />
     </div>
   )
 }
---
+++ app/Provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from 'react'
+import {ClientSideSuspense, LiveblocksProvider} from '@liveblocks/react/suspense';
+import {ReactNode} from 'react';
+import Loader from '../components/Loader';
+
+const Provider = ({children}: {children: ReactNode}) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <ClientSideSuspense fallback={<Loader />}>
+          {children}
+        </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default Provider
