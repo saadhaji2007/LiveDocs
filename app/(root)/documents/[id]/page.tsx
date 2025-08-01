ff9764d259a00273de073b3beca374d6ff9ccea0
Saad Haji <hajisaad029@gmail.com>
2025-08-01 11:22:39 +0530

    Implemented title feature to edit document

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import {redirect} from "next/navigation";
-import CollaborativeRoom from '@/components/CollaborativeRoom';
-import {currentUser} from "@clerk/nextjs/server";
-import {getDocument} from "@/lib/actions/room.actions";
+import CollaborativeRoom from "@/components/CollaborativeRoom"
+import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
 
-const Document = async ({params: { id }}: SearchParamProps) => {
-    const clerkUser = await currentUser();
-    if(!clerkUser) redirect('/sign-in');
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
 
-    const room = await getDocument({
-        roomId: id,
-        userId: clerkUser.emailAddresses[0].emailAddress,
-    });
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
 
-    if(!room) redirect('/');
+  if(!room) redirect('/');
+
 
     //todo: assess permisiion of user
-  return (
+return (
     <main className="flex w-full flex-col items-center">
-        <CollaborativeRoom 
+      <CollaborativeRoom 
         roomId={id}
         roomMetadata={room.metadata}
-        />
+      />
     </main>
   )
 }
