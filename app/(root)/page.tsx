b5dceab3b1dc21e64cca9e4a3ce7999c59e4722e
Saad Haji <hajisaad029@gmail.com>
2025-08-05 02:05:20 +0530

    Implemented Notifications Feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,8 @@ import {redirect} from 'next/navigation';
 import {getDocuments} from "@/lib/actions/room.actions";
 import Link from "next/link";
 import {dateConverter} from "@/lib/utils";
+import {DeleteModal} from '@/components/DeleteModal';
+import Notifications from '@/components/Notifiactions';
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -20,7 +22,7 @@ const Home = async () => {
       <main className="home-container">
         <Header className="sticky left-0 top-0 ">
           <div className="flex items-center gap-2 lg:gap-4">
-            Notification
+            <Notifications />
             <SignedIn>
               <UserButton />
             </SignedIn>
@@ -53,7 +55,7 @@ const Home = async () => {
                     <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                   </div>
                   </Link>
-                  {/* todo button */}
+                  <DeleteModal roomId={id} />
                 </li>
               ))}
             </ul>
