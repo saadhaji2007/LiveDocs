fc420388ee31fe1f8292dfcb356eb973264768d1
Saad Haji <hajisaad029@gmail.com>
2025-08-05 02:15:19 +0530

    build error fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
   /* config options here */
   images:{
     remotePatterns: [{protocol: 'https', hostname: 'img.clerk.com'}]
